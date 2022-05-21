bcdcecf171f9f2b3f8c8cba6743d3d559ad76de0
MeanMachineMarc <marcus9981@icloud.com>
2022-05-21 22:29:27 +0100

    Allowed for more inputs

--- The Fibonacci Sequence.py
+++ The Fibonacci Sequence.py
@@ -2,14 +2,19 @@ pt2=0
 pt1=1
 loop=0
 
-n=int(input("How many terms would you like to view (must be > 1)? "))
-n=n-2
-print(pt2)
-print(pt1)
-t=pt2+pt1
-while loop<n:
-    print(t)
-    pt2=pt1
-    pt1=t
-    t=pt2+pt1
-    loop=loop+1
+while True:
+    n=int(input("How many terms would you like to view? "))
+    n=n-2
+    print(pt2)
+    print(pt1)
+    if n>2:
+        t=pt2+pt1
+        while loop<n:
+            print(t)
+            pt2=pt1
+            pt1=t
+            t=pt2+pt1
+            loop=loop+1
+    else:
+        pass
+
